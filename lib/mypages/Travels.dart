9df1d96104079c126aecd04060113088ad30220e
cailavillostas <cailavillostas@gmail.com>
2025-06-30 11:46:29 +0800

    List items

---
+++ lib/mypages/Travels.dart
@@ -0,0 +1,15 @@
+class Travels{
+  String destination;
+  String startDate;
+  String endDate;
+  double budget;
+  String vehicle;
+
+  Travels({
+    required this. destination,
+    required this.startDate,
+    required this.endDate,
+    required this.budget,
+    required this.vehicle
+});
+}
